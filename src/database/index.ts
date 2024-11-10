31813870e1cb92dd77442d52d44c3cd8d4907a10
postassiums <batataprogramadora@gmail.com>
2024-11-10 10:39:36 -0300

    :bug: fix: Log error when failed to connect to database

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,10 +8,20 @@ const DATABASE_NAME=Deno.env.get('DATABASE_NAME')
 const DATABASE_HOST=Deno.env.get('DATABASE_HOST')
 const DATABASE_PORT=Deno.env.get('DATABASE_PORT')
 
-const URL=`mongodb://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}}`
+const URL=`mongodb://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:${DATABASE_PORT}`
 
+async function getDatabaseConnection()
+{
+	try{
+		return await moongose.connect(URL,{socketTimeoutMS: 15000,dbName: DATABASE_NAME})
+	}catch(e)
+	{
+		console.error(pallete.red('It was not possible to connect to the database ',e))
+		console.warn(pallete.yellow(`Connection string is: ${URL}`))
+		Deno.exit(1)
+	}
+}
 
+const db= await getDatabaseConnection()
 
-
-const db= await moongose.connect(URL)
 export default db
