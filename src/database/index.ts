761004e493851e9d7da90add836d8365c22edc44
postassiums <batataprogramadora@gmail.com>
2024-11-10 14:33:43 -0300

    :bug: fix: Timeout when connecting to database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,9 @@
-import {MongoClient, ObjectId} from 'npm:mongodb@6.10.0'
+import {MongoClient} from 'npm:mongodb@6.10.0'
 import { pallete } from "../helpers/index.ts";
-import { editora_collection, livro_collection } from "./schema.ts";
+import { sleep } from "https://deno.land/x/sleep/mod.ts"
 
 
-
-
-function getDatabaseConnection()
+async function getDatabaseConnection()
 {
 	const DATABASE_USER=Deno.env.get('DATABASE_USER')
 	const DATABASE_PASSWORD=Deno.env.get('DATABASE_PASSWORD')
@@ -14,7 +12,11 @@ function getDatabaseConnection()
 	const DATABASE_PORT=Deno.env.get('DATABASE_PORT')
 	const URL=`mongodb://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}`
 	try{
-		const client=new MongoClient(URL)
+		const TIMEOUT=6*10**3
+		// console.warn(pallete.yellow('Sleeping...'))
+		// await sleep(10)
+		// console.warn(pallete.yellow('End of sleeping'))
+		const client=new MongoClient(URL,{connectTimeoutMS: TIMEOUT,socketTimeoutMS: TIMEOUT })
 		return client.db(DATABASE_NAME)
 	}catch(e)
 	{
@@ -24,7 +26,7 @@ function getDatabaseConnection()
 	}
 }
 
-const db=  getDatabaseConnection()
+const db=await  getDatabaseConnection()
 
 
 
